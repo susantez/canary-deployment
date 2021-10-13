6ad573b8ffca3008cc085de0a5a125ed94c3f042
susantez <serkan.susantez@gmail.com>
2021-10-13 11:10:42 +0300

    added delay and error endpoints to enrich test cases

--- src/main/java/com/dfx/advanced/deployment/rest/impl/DefaultControllerImpl.java
+++ src/main/java/com/dfx/advanced/deployment/rest/impl/DefaultControllerImpl.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 public class DefaultControllerImpl implements DefaultController {
 
@@ -15,9 +17,42 @@ public class DefaultControllerImpl implements DefaultController {
 
     @Value("${app.version}")
     private String appVersion;
+    private static boolean SLOW_RESPONSE = false;
+    private static boolean RANDOM_EXCEPTIONS = false;
 
     @Override
-    public ResponseEntity<String> getVersion() {
-        return new ResponseEntity<>(appVersion, HttpStatus.OK);
+    public ResponseEntity<String> getVersion() throws Exception {
+        HttpStatus status = HttpStatus.OK;
+        if (SLOW_RESPONSE) {
+            int rand = (int)(Math.random() * 10);
+            try {
+                Thread.sleep(rand * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (RANDOM_EXCEPTIONS) {
+            int rand = (int)(Math.random() * 10);
+            if (rand > 5) {
+                status = HttpStatus.BAD_REQUEST;
+            }
+            if (rand > 8) {
+                throw new Exception("Custom Exception");
+            }
+        }
+        return new ResponseEntity<>(appVersion, status);
+    }
+
+    @Override
+    public ResponseEntity<Boolean> setDelay(@Valid Boolean state) {
+        this.SLOW_RESPONSE = state;
+        return new ResponseEntity<>(this.SLOW_RESPONSE, HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<Boolean> setError(@Valid Boolean state) {
+        this.RANDOM_EXCEPTIONS = state;
+        return new ResponseEntity<>(this.RANDOM_EXCEPTIONS, HttpStatus.OK);
     }
 }
